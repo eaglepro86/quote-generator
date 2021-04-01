08e478d9c9ee335c23a90f29c87afa2f8ca6bb44
AJ WRIGHT <solo@adam.home>
2021-04-01 23:43:58 +0100

    added api

---
+++ main.js
@@ -0,0 +1,34 @@
+// get quotes from api with asynchronous fetch request 
+
+let apiQuotes = [];
+
+// Show new quote
+function newQuote() {
+
+    // Pick a random quote from the apiQuotes array
+   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+   console.log(quote);
+}
+
+
+
+
+// Get the quotes
+async function getQuotes() {
+
+    // API url
+    const apiUrl = 'https://type.fit/api/quotes';
+
+    try {
+
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+
+    } catch (error) {
+
+        // Handle error
+
+    }
+}
+
+// getQuotes();
